51aa2ce7e6aa343c00248a5fa4ffca2cc765890e
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-03-26 14:40:10 -0700

    Add clear button

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-// let a = +prompt("enter a number");
-// let b = +prompt("enter another number");
+const button = document.querySelectorAll('.buttons button');
+const display = document.querySelector('.display'); 
+const clear = document.querySelector('.clear button');
+let displayNum;
+let displayOp;
+
+// Display character when clicked and store the values;
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function(e) {
+    let operators = ['+', '-', '*', '/'];
+    if (operators.includes(button[i].innerHTML)) {
+      displayOp = button[i].innerHTML;
+    } else {
+      displayNum = button[i].innerHTML;
+    }
+    display.innerHTML += button[i].innerHTML;
+  });
+};
+
+// clear button
+clear.addEventListener('click', function(e) {
+  display.innerHTML = '';
+})
 
 
+// these are all of the math functions
 const operate = function(operator, a, b) {
-  if (operator === "+") return add(a, b);
-  if (operator === "-") return subtract(a, b);
-  if (operator === "*") return multiply(a, b);
-  if (operator === "/") return divide(a, b);
+  if (operator === '+') return add(a, b);
+  if (operator === '-') return subtract(a, b);
+  if (operator === '*') return multiply(a, b);
+  if (operator === '/') return divide(a, b);
 };
 
 
@@ -28,5 +50,3 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
   return a / b;
 };
-
-// console.log(operate("+", a, b));
