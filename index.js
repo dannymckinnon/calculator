f7b8efcc79617a8718df43582cb3dbb5b69ba3a4
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-03-27 15:29:33 -0700

    Wire up operate function to the display

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const button = document.querySelectorAll('.buttons button');
 const display = document.querySelector('.display'); 
 const clear = document.querySelector('.clear button');
-let displayNum;
-let displayOp;
+let displayNum1 = '';
+let displayNum2 = '';
+let displayOp = '';
 
 // Display character when clicked and store the values;
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener('click', function(e) {
     let operators = ['+', '-', '*', '/'];
-    if (operators.includes(button[i].innerHTML)) {
-      displayOp = button[i].innerHTML;
+    
+    if (button[i].innerHTML === '=') {
+      let result = operate(displayOp, displayNum1, displayNum2);
+      display.innerHTML = result;
+      displayNum1 = result.toString();
+      displayNum2 = '';
+      displayOp = '';
+    };
+
+    if (displayOp.length === 0) {
+      if (operators.includes(button[i].innerHTML)) {
+        displayOp = button[i].innerHTML;
+      } else if (button[i].innerHTML != '=') {
+        displayNum1 += button[i].innerHTML;
+      }
     } else {
-      displayNum = button[i].innerHTML;
-    }
-    display.innerHTML += button[i].innerHTML;
+      displayNum2 += button[i].innerHTML;
+    };
+
+    if (button[i].innerHTML != '=') {
+      display.innerHTML += button[i].innerHTML;
+    };
+    console.log(displayNum1);
+    console.log(displayOp);
+    console.log(displayNum2);
   });
 };
 
 // clear button
 clear.addEventListener('click', function(e) {
   display.innerHTML = '';
+  displayNum1 = '';
+  displayNum2 = '';
+  displayOp = '';
 })
 
 
