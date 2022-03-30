2011f90801a9f9991e02677ddd4da3f7ce40dbc1
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-03-29 17:01:11 -0700

    Redesign interface

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const button = document.querySelectorAll('.buttons button');
 const display = document.querySelector('.display'); 
-const clear = document.querySelector('.clear button');
+const clear = document.querySelector('.clear');
 let displayNum1 = '';
 let displayNum2 = '';
 let displayOp = '';
@@ -22,14 +22,14 @@ for (let i = 0; i < button.length; i++) {
       if (operators.includes(button[i].innerHTML)) {
         displayOp = button[i].innerHTML;
       } else if (button[i].innerHTML != '=') {
-        displayNum1 += button[i].innerHTML;
+        displayNum1 = button[i].innerHTML;
       }
     } else {
-      displayNum2 += button[i].innerHTML;
+      displayNum2 = button[i].innerHTML;
     };
 
     if (button[i].innerHTML != '=') {
-      display.innerHTML += button[i].innerHTML;
+      display.innerHTML = button[i].innerHTML;
     };
     console.log(displayNum1);
     console.log(displayOp);
@@ -39,7 +39,7 @@ for (let i = 0; i < button.length; i++) {
 
 // clear button
 clear.addEventListener('click', function(e) {
-  display.innerHTML = '';
+  display.innerHTML = 0;
   displayNum1 = '';
   displayNum2 = '';
   displayOp = '';
