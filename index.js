49c509542f8d38af1e0302cef74b0f1cf8789cc0
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-03-31 20:46:29 -0700

    Add decimal button functionality

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const numbers = document.querySelectorAll('.numbers');
-const operators = document.querySelectorAll('.operators');
 const display = document.querySelector('.display'); 
-const clear = document.querySelector('.clear');
 
 let num1 = '0';
 let operator = '';
 let num2 = '0';
 
 
+// When a number button is clicked, store the number
+const numbers = document.querySelectorAll('.numbers');
 
-// Add event listener to number buttons and store what button was pressed
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener('click', () => {
     let selection = numbers[i].innerHTML;
@@ -34,21 +32,58 @@ for (let i = 0; i < numbers.length; i++) {
   });
 } 
 
-// Add event listener to operator buttons and store what button was pressed
+
+// When an operator button is clicked, store the operator
+const operators = document.querySelectorAll('.operators');
+
 for (let i = 0; i < operators.length; i++) {
   operators[i].addEventListener('click', () => {
     let selection = operators[i].innerHTML;
+    
+    if (+num1 > 0 && +num2 > 0) {
+      let result = operate(operator, +num1, +num2);
+      num1 = result.toString();
+      num2 = '0';
+      operator = '';
+      display.innerHTML = result.toString();
+    }  
     operator = selection;
-
+    
     console.log(num1, operator, num2);
   });
 } 
 
 
+// When the equals button is clicked, evaluate the equation 
+const equals = document.querySelector('.equals');
 
+equals.addEventListener('click', () => {
+  if (operator) {
+    let result = operate(operator, +num1, +num2);
+    (result === NaN) ? display.innerHTML = NaN : display.innerHTML = result.toString();
+    num1 = '0'
+    num2 = '0';
+    operator = '';
+    display.innerHTML = result.toString();
+    console.log(num1, operator, num2);
+  }
+});
+
+
+// decimal button
+const decimal = document.querySelector('.decimal');
+
+decimal.addEventListener('click', () => {
+  if ( !(display.innerHTML.includes('.')) ) {
+    display.innerHTML += '.';
+    (operator) ? (num2 += '.') : (num1 += '.');
+  }
+});
 
 
 // clear button
+const clear = document.querySelector('.clear');
+
 clear.addEventListener('click', () => {
   display.innerHTML = 0;
   num1 = '0';
@@ -59,6 +94,7 @@ clear.addEventListener('click', () => {
 
 
 const operate = function(operator, a, b) {
+  if (b === 0 && operator === '/') return NaN;
   if (operator === '+') return a + b;
   if (operator === '-') return a - b;
   if (operator === '*') return a * b;
